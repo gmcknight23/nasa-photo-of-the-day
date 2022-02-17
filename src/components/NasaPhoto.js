7f1c922e01765e72cf7c445e6d6132b93000c6f0
gregmcknight <gmcknight23@gmail.com>
2022-02-17 15:50:39 -0500

    DONE1

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,22 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledDetails = styled.div`
+  background-color: skyblue;
+  color: darkred;
+`;
 
 const NasaPhoto = (props) => {
   return (
-    <div className="nasa-photo-wrapper">
+    <StyledDetails className="nasa-photo-wrapper">
       <h3>{props.photo.title}</h3>
       <p>{props.photo.date}</p>
-      <iframe
-        src={props.photo.url}
-        frameborder="0"
-        height="500"
-        width="600"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="video"
-      />
+      <img src={props.photo.hdurl} alt="nasa-APOD" />
       <p className="explanation">({props.photo.explanation} </p>
-    </div>
+    </StyledDetails>
   );
 };
 
 export default NasaPhoto;
+
+// {
+//   <iframe
+//     src={props.photo.url}
+//     frameborder="0"
+//     height="500"
+//     width="600"
+//     allow="autoplay; encrypted-media"
+//     allowFullScreen
+//     title="video"
+//   />;
+// }
